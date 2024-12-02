0607f9534349723ac0035dbf1b64beb8a4d5492e
codewithryry <reymelrey.mislang@gmail.com>
2024-12-02 09:04:24 +0800

    more dataset

--- runserver.py
+++ runserver.py
@@ -1,5 +1,5 @@
 from waitress import serve
-from jobfit.wsgi import application
+from jobfit.wsgi import application  
 
-if __name__ == "__main__":
-    serve(application, host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    serve(application, host='0.0.0.0', port=5000)
